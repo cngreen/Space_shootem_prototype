1dc0e7d20976f5d39d81972d3328f294856b4963
Kari Green <Kari@Karis-MBP.hsd1.mi.comcast.net>
2017-01-07 21:21:36 -0500

    Utils bounds update

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -11,8 +11,11 @@ public class Hero : MonoBehaviour {
 	public float shieldLevel = 1;
 	public bool ______________________;
 
+	public Bounds bounds;
+
 	void Awake(){
 		S = this;
+		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
 	}
 
 	void Update(){
@@ -23,6 +26,13 @@ public class Hero : MonoBehaviour {
 		pos.x += xAxis * speed * Time.deltaTime;
 		pos.y += yAxis * speed * Time.deltaTime;
 		transform.position = pos;
+
+		bounds.center = transform.position;
+		Vector3 off = Utils.ScreenBoundsCheck (bounds, BoundsTest.onScreen);
+		if (off != Vector3.zero) {
+			pos -= off;
+			transform.position = pos;
+		}
 
 		transform.rotation = Quaternion.Euler (yAxis * pitchMult, xAxis * rollMult, 0);
 	}
