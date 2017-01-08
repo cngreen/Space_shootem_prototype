af7214290fa0891e0e523de5073123ed5c438ef9
Kari Green <Kari@Karis-MBP.hsd1.mi.comcast.net>
2017-01-08 11:44:47 -0500

    weapons on hero

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -12,6 +12,9 @@ public class Hero : MonoBehaviour {
 
 	[SerializeField]
 	private float _shieldLevel = 1;
+
+	public Weapon[] weapons; 
+
 	public bool ______________________;
 
 	public Bounds bounds;
@@ -21,6 +24,9 @@ public class Hero : MonoBehaviour {
 	void Awake(){
 		S = this;
 		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
+
+		ClearWeapons ();
+		weapons [0].SetType (WeaponType.blaster);
 	}
 
 	void Update(){
@@ -60,11 +66,53 @@ public class Hero : MonoBehaviour {
 			if (go.tag == "Enemy") {
 				shieldLevel--;
 				Destroy (go);
-			} else {
+			} else if(go.tag == "PowerUp"){
+				AbsorbPowerUp (go);
+			}else {
 				print ("Triggered: " + go.name);
 			}
 		} else {
 			print ("Triggered: " + other.gameObject.name);
+		}
+	}
+
+	public void AbsorbPowerUp(GameObject go){
+		PowerUp pu = go.GetComponent<PowerUp> ();
+		switch (pu.type) {
+		case WeaponType.shield:
+			shieldLevel++;
+			break;
+
+		default:
+			if (pu.type == weapons [0].type) {
+				Weapon w = GetEmptyWeaponSlot ();
+				if (w != null) {
+					w.SetType (pu.type);
+				}
+			} else {
+				ClearWeapons ();
+				weapons [0].SetType (pu.type);
+			}	
+
+			break;
+		}
+
+		pu.AbsorbedBy (this.gameObject);
+	}
+
+	Weapon GetEmptyWeaponSlot(){
+		for (int i = 0; i < weapons.Length; i++) {
+			if (weapons [i].type == WeaponType.none) {
+				return (weapons [i]);
+			}
+		}
+
+		return(null);
+	}
+
+	void ClearWeapons(){
+		foreach (Weapon w in weapons) {
+			w.SetType (WeaponType.none);
 		}
 	}
 
